2c39a209f11a9afd615fcbfd1b2695489d971ab1
helga-umrikh <zetaumr@gmail.com>
2024-03-16 13:15:56 +0300

    feat: add colums for cart and total amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import {
+    SplitLayout,
+    View,
+    Panel,
+    PanelHeader,
+    Header,
+    Group,
+    SplitCol,
+    Div,
+} from '@vkontakte/vkui'
+import '@vkontakte/vkui/dist/vkui.css';
 
-function App() {
-    return <div className="App"></div>
+const App = () => {
+    return (
+        <div className="App">
+            <View activePanel="header">
+                <Panel id="header">
+                    <PanelHeader>Корзина</PanelHeader>
+                    <Div>
+                        <SplitLayout>
+                            <SplitCol
+                                width={3}
+                                style={{ paddingRight: '16px' }}
+                            >
+                                <Group
+                                    header={
+                                        <Header mode="secondary">
+                                            Содержимое корзины:
+                                        </Header>
+                                    }
+                                    padding={'m'}
+                                >
+                                    
+                                </Group>
+                            </SplitCol>
+                            <SplitCol
+                                width={1}
+                                style={{ paddingRight: '16px' }}
+                            >
+                                <Group
+                                    header={
+                                        <Header mode="secondary">
+                                            Оформление заказа:
+                                        </Header>
+                                    }
+                                >
+                                    <Header>Итого сумма 5999&nbsp;₽</Header>
+                                </Group>
+                            </SplitCol>
+                        </SplitLayout>
+                    </Div>
+                </Panel>
+            </View>
+        </div>
+    )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+const root = createRoot(container!); 
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <App />
+    </AdaptivityProvider>
+  </ConfigProvider>
 );
